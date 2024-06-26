99e0a36e6199fb213201cc5560aee406b33f9e8c
smritiramann <smritiramann@gmail.com>
2024-06-27 00:18:30 +0530

    custom.js

--- custom.js
+++ custom.js
@@ -1,21 +1,3 @@
-# My Custom NFT
-
-A simple javascript program to demonstrate the use of creating a local nft variable to store nft data in it by calling the mintNFT function by passing the required parameters for it, listNFT function which will loop through the nft variable and print the metadata and finally the getTotalSupply function to list the nft length till now stored in the nft variable. 
-
-## Description
-
-The program features the following things -
-1. variables to store the nft details
-2. a mintNFt function which will accept the metadata and store it in the array
-3. getTotalSupply function to print the amount of nft we have minted till now
-
-## Getting Started
-
-### Executing program
-
-To run this program, you can use online javascript compiler or vs code if you have node installed locally. Just copy paste the content of the Custom.js file and run it in the online IDE. 
-
-```javascript
 /*
 Assessment Requirements
 1. Create a variable that can hold a number of NFT's. What type of variable might this be?
@@ -61,10 +43,3 @@ mintNFT("Himalayan","adventure bike","411cc","345kmph");
 listNFTs();
 getTotalSupply();
 ```
-## Authors
-
-Smriti Raman
-
-## License
-
-This project is licensed under the MIT License - see the LICENSE.md file for details
